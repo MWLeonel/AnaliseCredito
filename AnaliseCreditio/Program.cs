237e927c1707606f10f43efececa19a8af505dcf
MWLeonel <wmleonel6@gmail.com>
2020-10-12 20:13:13 -0300

    Analisador de Emprestimo

--- AnaliseCreditio/Program.cs
+++ AnaliseCreditio/Program.cs
@@ -13,40 +13,64 @@ namespace AnaliseCreditio
 
             Console.Write("Digite o Valor Desejado: ");
             valorValido = Decimal.TryParse(Console.ReadLine(), out valor);
+            
+            if (!valorValido)
+            {
+                Console.WriteLine();
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Você digitou um valor invalido");
+                Console.WriteLine("Pressione ENTER para sair, e execute o programa novamente");
+                Console.ReadKey();
+                Console.ResetColor();
+                Environment.Exit(-1);
+            }
 
             Console.Write("Digite a Quantidade de Parcelas Desejada: ");
             parcelasValido = int.TryParse(Console.ReadLine(), out parcelas);
+
+            if (!parcelasValido)
+            {
+                Console.WriteLine();
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Você digitou um valor invalido");
+                Console.WriteLine("Pressione ENTER para sair, e execute o programa novamente");
+                Console.ReadKey();
+                Console.ResetColor();
+                Environment.Exit(-1);
+            }
 
             Console.Write("Digite a Sua Renda Mensal: ");
             rendaMensalValido = Decimal.TryParse(Console.ReadLine(), out rendaMensal);
             Console.WriteLine();
 
-            if (valorValido && parcelasValido && rendaMensalValido)
-            {
-                valorParcela = valor / parcelas;
-                valorMaximoParcela = rendaMensal * 0.3m;
-
-                Console.WriteLine($"Sua Quantidade de Parcelas: {valorParcela:C}");
-                Console.WriteLine($"Parcela Maxima: {valorMaximoParcela:C}");
-
-                if (valorParcela <= valorMaximoParcela)
-                {
-                    Console.WriteLine();
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine("Parabens, seu credito foi aprovado e aproveite bem o seu dinheiro :)");
-                }
-                else
-                {
-                    Console.WriteLine();
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Sinto muito, seu credito não foi aprovado, tente numa proxima vez :(");
-                }
-            }
-            else
+            if (!rendaMensalValido)
             {
                 Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine("Você digitou algum valor inválido. Tente novamente.");
+                Console.WriteLine("Você digitou um valor invalido");
+                Console.WriteLine("Pressione ENTER para sair, e execute o programa novamente");
+                Console.ReadKey();
+                Console.ResetColor();
+                Environment.Exit(-1);
             }
+
+             valorParcela = valor / parcelas;
+             valorMaximoParcela = rendaMensal * 0.3m;
+
+             Console.WriteLine($"Sua Quantidade de Parcelas: {valorParcela:C}");
+             Console.WriteLine($"Parcela Maxima: {valorMaximoParcela:C}");
+
+             if (valorParcela <= valorMaximoParcela)
+             {
+                  Console.WriteLine();
+                  Console.ForegroundColor = ConsoleColor.Green;
+                  Console.WriteLine("Parabens, seu credito foi aprovado e aproveite bem o seu dinheiro :)");
+             }
+             else
+             {
+                  Console.WriteLine();
+                  Console.ForegroundColor = ConsoleColor.Red;
+                  Console.WriteLine("Sinto muito, seu credito não foi aprovado, tente em uma proxima vez :(");
+             }
 
             Console.WriteLine();
             Console.ResetColor();
